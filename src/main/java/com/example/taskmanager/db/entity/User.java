190fa5c0948f147c3cbf1fe245af143501c2c520
Filip Kasprzycki <kasprzycki.filip@gmail.com>
2025-01-03 13:46:17 +0100

    TasksApi - Add SuperEntity class. It is an abstract class with id, createdAt and updatedAt fields and prePersist and preUpdate methods. Entity class can extends from SuperEntity

--- src/main/java/com/example/taskmanager/db/entity/User.java
+++ src/main/java/com/example/taskmanager/db/entity/User.java
@@ -4,7 +4,6 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Getter
@@ -16,13 +15,9 @@ import java.util.UUID;
                 @UniqueConstraint(columnNames = "email")
         })
 @NamedQuery(name = User.FIND_BY_EMAIL, query = "SELECT u FROM User u WHERE u.email = :email")
-public class User {
+public class User extends SuperEntity {
 
     public final static String FIND_BY_EMAIL = "User.FindByEmail";
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
 
     private UUID uuid;
 
@@ -32,18 +27,8 @@ public class User {
 
     private String salt;
 
-    private LocalDateTime createAt;
-
-    private LocalDateTime updatedAt;
-
     @PrePersist
-    private void prePersist() {
+    void generateUuid() {
         this.uuid = UUID.randomUUID();
-        this.createAt = LocalDateTime.now();
-    }
-
-    @PreUpdate
-    private void preUpdate() {
-        this.updatedAt = LocalDateTime.now();
     }
 }
