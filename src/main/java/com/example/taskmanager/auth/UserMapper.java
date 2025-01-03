921ae933f6231a19bf9809feafcc8442d8dcc89b
Filip Kasprzycki <kasprzycki.filip@gmail.com>
2025-01-03 12:12:20 +0100

    TasksApi - Fix user registration process - use trim and toLowerCase email when creating new user

--- src/main/java/com/example/taskmanager/auth/UserMapper.java
+++ src/main/java/com/example/taskmanager/auth/UserMapper.java
@@ -1,13 +1,12 @@
 package com.example.taskmanager.auth;
 
-import com.example.taskmanager.auth.entity.AuthApiRequest;
 import com.example.taskmanager.db.entity.User;
 
 class UserMapper {
 
-    User map(AuthApiRequest request, String password, String salt) {
+    User map(String email, String password, String salt) {
         User user = new User();
-        user.setEmail(request.getEmail());
+        user.setEmail(email);
         user.setPassword(password);
         user.setSalt(salt);
         return user;
